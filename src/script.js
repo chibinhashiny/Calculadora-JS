743c6bf59981705649b71a93d23258a3bf154d73
chibinhashiny <bcavalcante992@gmail.com>
2025-02-02 22:23:08 -0300

    musica adicionada e umas coisas na UI tbm

--- src/script.js
+++ src/script.js
@@ -5,35 +5,76 @@ let OperationType = prompt("escolha uma operação (+/add/1 - adição, -/sub/2 
 let N1Int = parseInt(N1);
 let N2Int = parseInt(N2);
 let V;
+let AddBTN = document.getElementById("add");
+let SubBTN = document.getElementById("sub");
+let MultiBTN = document.getElementById("mult");
+
+let DivBTN = document.getElementById("div");
+
 
 document.querySelector('#header').innerHTML = message
 
-console.log(message)
-if(OperationType == "+" || OperationType == "add" ||  OperationType == "1")
+AddBTN.addEventListener("click", Add);
+SubBTN.addEventListener("click", Sub);
+MultiBTN.addEventListener("click", Multi);
+DivBTN.addEventListener("click", Div);
+
+
+
+function Add()
 {
-   V=N1Int + N2Int
-   alert("o resultado é:" +V);
+  V=N1Int + N2Int
+  alert("o resultado é:" +V);
+  atualizarResultado();
 }
-else if(OperationType == "-"|| OperationType == "sub" || OperationType == "2")
+
+function Sub()
 {
   V=N1Int - N2Int
   alert("o resultado é:" +V);
+  atualizarResultado();
 }
-else if(OperationType == "*"||  OperationType =="multi" || OperationType == "3")
+
+
+function Multi()
 {
   V=N1Int * N2Int
   alert("o resultado é:" +V);
+  atualizarResultado();
 }
-else if(OperationType == "/" || OperationType == "div" || OperationType == "4")
+
+
+function Div()
 {
   if(N2 !== "0"){
     V=N1Int / N2Int
   alert("o resultado é:" +V);
+  atualizarResultado();
   }
   else
   {
     alert("nao pode dividir por zero!! ! !")
   }
+}
+
+
+
+console.log(message)
+if(OperationType == "+" || OperationType == "add" ||  OperationType == "1")
+{
+  Add();
+}
+else if(OperationType == "-"|| OperationType == "sub" || OperationType == "2")
+{
+  Sub();
+}
+else if(OperationType == "*"||  OperationType =="multi" || OperationType == "3")
+{
+  Multi();
+}
+else if(OperationType == "/" || OperationType == "div" || OperationType == "4")
+{
+ Div();
   
 }
 
@@ -45,8 +86,14 @@ else if(!["+", "-", "*", "/"].includes(OperationType))
 
 
 console.log(V);
-document.querySelector('#header').innerHTML = "resultado:" + V; 
-document.querySelector('#paragrafo').innerHTML = "A CALCULADORA MAIS INSANA DOS SETE MARES";
+
+
+function atualizarResultado() {
+  document.querySelector('#header').innerHTML = "Resultado: " + V;
+  document.querySelector('#paragrafo').innerHTML = "A CALCULADORA MAIS INSANA DOS SETE MARES";
+}
+
+
 
 
 
